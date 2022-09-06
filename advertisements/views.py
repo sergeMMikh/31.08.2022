22e0c5978b12bdd5bbb5f9950a564ac341d3aede
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-09-06 16:20:58 +0100

    added IsOwnerOrReadOnly permission

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,5 +1,6 @@
 from django_filters import DateFromToRangeFilter
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -13,6 +14,23 @@ class DataFilter(FilterSet):
     class Meta:
         model = Advertisement
         exclude = []
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.creator == request.user
+
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
@@ -30,5 +48,8 @@ class AdvertisementViewSet(ModelViewSet):
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
-        return []
+            self.permission_classes = [IsAuthenticated()]
+        if self.action in ["destroy", ]:
+            self.permission_classes = [IsOwnerOrReadOnly]
+
+        return super(self.__class__, self).get_permissions()
