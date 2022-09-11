0d404633d7cfdbcc83ef3bb1ec3556962d207832
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-09-11 08:38:06 +0100

    added filters.py and permissions.py

--- advertisements/filters.py
+++ advertisements/filters.py
@@ -1,12 +1,16 @@
 from django_filters import rest_framework as filters
-
+from django_filters import DateFromToRangeFilter
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
-    # TODO: задайте требуемые фильтры
+    created_at = DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        exclude = []
 
     class Meta:
         model = Advertisement
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,34 +1,12 @@
-from django_filters import DateFromToRangeFilter
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet
-from rest_framework import permissions
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
-
-
-class DataFilter(FilterSet):
-    created_at = DateFromToRangeFilter()
-
-    class Meta:
-        model = Advertisement
-        exclude = []
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance must have an attribute named `owner`.
-        return obj.creator == request.user
+from .filters import AdvertisementFilter
+from .permissions import IsOwnerOrReadOnly
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -38,14 +16,14 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
 
     filter_backends = [DjangoFilterBackend, ]
-    filterset_class = DataFilter
+    filterset_class = AdvertisementFilter
     search_fields = ['creator', 'status', 'created_at', ]
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "partial_update"]:
             self.permission_classes = [IsAuthenticated]
-        if self.action in ["destroy", ]:
+        if self.action in ["destroy", "update", ]:
             self.permission_classes = [IsOwnerOrReadOnly]
 
         return super(self.__class__, self).get_permissions()
